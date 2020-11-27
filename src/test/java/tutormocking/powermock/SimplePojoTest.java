3a83439b5d32c28808ac97b832f35055281cd844
vachad <daniel.vacha@kctdata.cz>
2020-11-27 16:05:37 +0100

    priprava pro prezentaci docesavani kodu,

--- src/test/java/tutormocking/powermock/SimplePojoTest.java
+++ src/test/java/tutormocking/powermock/SimplePojoTest.java
@@ -6,8 +6,7 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.logging.Logger;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 import static org.powermock.api.mockito.PowerMockito.spy;
 import static org.powermock.api.mockito.PowerMockito.when;
 
@@ -26,12 +25,12 @@ public class SimplePojoTest {
      */
     @Test
     public void mockPrivateFunction() throws Exception {
-        SimplePojo hlavniTrida = new SimplePojo();
-        SimplePojo mock = spy(hlavniTrida);
+        SimplePojo simplePojo = new SimplePojo();
+        SimplePojo spyMock = spy(simplePojo);
         String expectedResult="String to je inkrement 0 mockovany private";
-        // mock privatni metody pomoci powermocku
-        when(mock, "getInfoAndVersion").thenReturn(" mockovany private");
-        String result = mock.processing();
+        // spyMock privatni metody pomoci powermocku
+        when(spyMock, "getInfoAndVersion").thenReturn(" mockovany private");
+        String result = spyMock.processing();
         LOGGER.info(" processing "+result);
         assertEquals(expectedResult, result);
     }
